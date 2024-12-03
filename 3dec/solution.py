4f4692bdda34999ce0c36078ee780c458511ec6d
Violeta Malinova <violeta@mlnova.com>
2024-12-03 09:20:00 +0200

    simplify day 3

--- 3dec/solution.py
+++ 3dec/solution.py
@@ -1,23 +1,19 @@
 import re
 
-lines = []
-with open("example.txt") as f:
-    lines = f.read().splitlines()
+with open("test.txt") as f:
+    seq = f.read()
+
+matches = re.findall(
+    r'(mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don\'t\(\))', seq)
 
 sum = 0
-
-for line in lines:
-
-    matches = re.findall(
-        r'(mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don\'t\(\))', line)
-
-    enabled = True
-    for m in matches:
-        if "do()" in m[0]:
-            enabled = True
-        elif "don't()" in m[0]:
-            enabled = False
-        elif enabled == True:
-            sum += int(m[1]) * int(m[2])
+enabled = True
+for m in matches:
+    if "do()" in m[0]:
+        enabled = True
+    elif "don't()" in m[0]:
+        enabled = False
+    elif enabled == True:
+        sum += int(m[1]) * int(m[2])
 
 print(sum)
