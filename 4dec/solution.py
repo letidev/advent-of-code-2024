df6eb00a13e5eeb142bb166bc1db038f73800ba0
Violeta Malinova <violeta@mlnova.com>
2024-12-04 09:20:00 +0200

    4 December Part 2

--- 4dec/solution.py
+++ 4dec/solution.py
@@ -49,4 +49,19 @@ for i in range(n):
             if lines[i][j] == "S" and lines[i+1][j-1] == "A" and lines[i+2][j-2] == "M" and lines[i+3][j-3] == "X":
                 total += 1
 
-print(total)
+print("part 1", total)
+
+# part 2
+
+# M . S
+# . A .
+# M . S
+
+total = 0
+for i in range(n):
+    for j in range(m):
+        if i + 2 < n and j + 2 < m:
+            if lines[i+1][j+1] == "A" and ((lines[i][j] == "M" and lines[i+2][j+2] == "S") or (lines[i][j] == "S" and lines[i+2][j+2] == "M")) and ((lines[i][j+2] == "M" and lines[i+2][j] == "S") or (lines[i][j+2] == "S" and lines[i+2][j] == "M")):
+                total += 1
+
+print("part 2", total)
