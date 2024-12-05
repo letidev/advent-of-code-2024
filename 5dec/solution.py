50158617e788402c29eee8b64c3c1bce9a7576aa
Violeta Malinova <violeta@mlnova.com>
2024-12-05 09:52:00 +0200

    5 December Part 2

--- 5dec/solution.py
+++ 5dec/solution.py
@@ -1,3 +1,4 @@
+import functools
 import math
 from typing import Dict
 
@@ -19,6 +20,18 @@ for rule in rules:
     rulemap[first].append(second)
 
 total = 0
+total2 = 0
+
+
+def sort_fn(a, b):
+    if a in rulemap and b in rulemap[a]:
+        return -1
+    elif b in rulemap and a in rulemap[b]:
+        return 1
+    else:
+        return 0
+
+
 for m in manuals:
     pages = m.split(",")
     size = len(pages)
@@ -44,5 +57,9 @@ for m in manuals:
 
     if is_ok:
         total += int(pages[math.ceil((size-1) / 2)])
+    else:  # part 2
+        fixed = sorted(pages, key=functools.cmp_to_key(sort_fn))
+        total2 += int(fixed[math.ceil((size-1) / 2)])
 
-print(total)
+print("part 1", total)
+print("part 2", total2)
