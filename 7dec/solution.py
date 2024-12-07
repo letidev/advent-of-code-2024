590230bc9940ae43ea5ce2baa0f62c1155eddce4
Violeta Malinova <violeta@mlnova.com>
2024-12-07 10:54:17 +0200

    7 Dec - remove unnecessary param and measure time

--- 7dec/solution.py
+++ 7dec/solution.py
@@ -1,27 +1,32 @@
+import time
+
 with open("test.txt") as f:
     lines = f.read().splitlines()
 
 
-def eq(n, pos, nums, tv, sum, calcs) -> bool:
+def eq(n, pos, nums, sum, calcs) -> bool:
     if pos == n:
         calcs.append(sum)
         return
 
-    eq(n, pos+1, nums, tv, sum + nums[pos], calcs)
-    eq(n, pos+1, nums, tv, sum * nums[pos], calcs)
-    eq(n, pos+1, nums, tv, int(f"{sum}{nums[pos]}"), calcs)
+    eq(n, pos+1, nums, sum + nums[pos], calcs)
+    eq(n, pos+1, nums, sum * nums[pos], calcs)
+    eq(n, pos+1, nums, int(f"{sum}{nums[pos]}"), calcs)
 
 
+start_time = time.time()
 total = 0
 for line in lines:
     tv, operation = line.split(": ")
     tv = int(tv)
     nums = [int(x) for x in operation.split(" ")]
+    n = len(nums)
 
     calcs = []
-    eq(len(nums), 1, nums, tv, nums[0], calcs)
+    eq(n, 1, nums, nums[0], calcs)
 
     if tv in calcs:
         total += tv
 
+print(time.time() - start_time, "seconds")
 print(total)
