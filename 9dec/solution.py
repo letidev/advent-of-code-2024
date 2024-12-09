8ccc5a16285fb13c207d1324835c75e73b8c3eb3
Violeta Malinova <violeta@mlnova.com>
2024-12-09 09:41:46 +0200

    9 December Part 2

--- 9dec/solution.py
+++ 9dec/solution.py
@@ -5,38 +5,47 @@ with open("test.txt") as f:
 
 fs = []
 fid = 0
+space_id = 0
+space_map = {}
+files_map = {}
 
 start_time = time.time()
-for i in range(len(diskmap)):
-    size = int(diskmap[i])
-    if i % 2 == 0:
+for f in range(len(diskmap)):
+    size = int(diskmap[f])
+    if f % 2 == 0:
+        files_map[fid] = {
+            'size': size,
+            'idx': len(fs)
+        }
         fs.extend([fid for _ in range(size)])
         fid += 1
     else:
+        space_map[space_id] = {
+            'size': size,
+            'idx': len(fs)
+        }
         fs.extend(["." for _ in range(size)])
+        space_id += 1
 
-n = len(fs)
-left = 0
-right = n-1
-
-while left < right:
-    if fs[left] == "." and fs[right] != ".":
-        fs[left] = fs[right]
-        fs[right] = "."
-        left += 1
-        right -= 1
-    else:
-        if fs[left] != ".":
-            left += 1
-        if fs[right] == ".":
-            right -= 1
+for f in reversed(range(fid)):
+    for s in range(space_id):
+        if space_map[s]['idx'] < files_map[f]['idx']:
+            if space_map[s]['size'] >= files_map[f]['size']:
+                left = space_map[s]['idx']
+                right = files_map[f]['idx']
+                for k in range(files_map[f]['size']):
+                    fs[left+k] = f
+                    fs[right+k] = "."
+                space_map[s]['size'] -= files_map[f]['size']
+                space_map[s]['idx'] += files_map[f]['size']
+                break
+        else:
+            break
 
 checksum = 0
-for i in range(n):
-    if fs[i] != ".":
-        checksum += i * fs[i]
-    else:
-        break
+for f in range(len(fs)):
+    if fs[f] != ".":
+        checksum += f * fs[f]
 
-print(time.time() - start_time, "seconds")
+print("Time:", time.time() - start_time, "seconds")
 print(checksum)
