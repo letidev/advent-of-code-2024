c3ebc5ef94ebbf78565e100ff940e2a7c18eb972
Violeta Malinova <violeta@mlnova.com>
2024-12-10 09:49:54 +0200

    10 December Part 2

--- 10dec/solution.py
+++ 10dec/solution.py
@@ -12,21 +12,34 @@ def get_score(n, m, i, j):
         dp_score[i][j] = 1
         return
 
-    if i > 0 and lines[i][j] + 1 == lines[i-1][j] and dp_score[i-1][j] == 0:
-        get_score(n, m, i-1, j)
-        dp_score[i][j] += dp_score[i-1][j]
+    if i > 0 and lines[i][j] + 1 == lines[i-1][j]:
+        if dp_score[i-1][j] != 0:
+            dp_score[i][j] += dp_score[i-1][j]
 
-    if i < n - 1 and lines[i][j] + 1 == lines[i+1][j] and dp_score[i+1][j] == 0:
-        get_score(n, m, i+1, j)
-        dp_score[i][j] += dp_score[i+1][j]
+        else:
+            get_score(n, m, i-1, j)
+            dp_score[i][j] += dp_score[i-1][j]
 
-    if j > 0 and lines[i][j] + 1 == lines[i][j-1] and dp_score[i][j-1] == 0:
-        get_score(n, m, i, j-1)
-        dp_score[i][j] += dp_score[i][j-1]
+    if i < n - 1 and lines[i][j] + 1 == lines[i+1][j]:
+        if dp_score[i+1][j] != 0:
+            dp_score[i][j] += dp_score[i+1][j]
+        else:
+            get_score(n, m, i+1, j)
+            dp_score[i][j] += dp_score[i+1][j]
 
-    if j < m - 1 and lines[i][j] + 1 == lines[i][j+1] and dp_score[i][j+1] == 0:
-        get_score(n, m, i, j+1)
-        dp_score[i][j] += dp_score[i][j+1]
+    if j > 0 and lines[i][j] + 1 == lines[i][j-1]:
+        if dp_score[i][j-1] != 0:
+            dp_score[i][j] += dp_score[i][j-1]
+        else:
+            get_score(n, m, i, j-1)
+            dp_score[i][j] += dp_score[i][j-1]
+
+    if j < m - 1 and lines[i][j] + 1 == lines[i][j+1]:
+        if dp_score[i][j+1] != 0:
+            dp_score[i][j] += dp_score[i][j+1]
+        else:
+            get_score(n, m, i, j+1)
+            dp_score[i][j] += dp_score[i][j+1]
 
 
 lines = [[int(line[i]) for i in range(m)] for line in lines]
@@ -37,8 +50,12 @@ for i in range(n):
             heads.append((i, j))
 
 for head in heads:
-    dp_score = [[0 for _ in range(m)] for _ in range(n)]
     get_score(n, m, head[0], head[1])
     scores += dp_score[head[0]][head[1]]
+
+# for i in range(n):
+#     for j in range(m):
+#         print(dp_score[i][j], end='  ')
+#     print()
 
 print(scores)
