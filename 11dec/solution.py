0c0f6b04322150398e250d5df73dcfdb6f43b048
Violeta Malinova <violeta@mlnova.com>
2024-12-11 11:21:27 +0200

    11 December Parts 1 and 2 together

--- 11dec/solution.py
+++ 11dec/solution.py
@@ -3,24 +3,41 @@ import time
 with open("test.txt") as f:
     stones = f.read().split()
 
+dp: list[dict[str, int]] = [{} for _ in range(100)]
+
+
+def count(stone: str, blink: int) -> int:
+    if blink == 0:
+        return 1
+
+    memo = dp[blink]
+
+    if stone in memo:
+        return memo[stone]
+
+    if stone == '0':
+        memo[stone] = count('1', blink-1)
+    elif len(stone) % 2 == 0:
+        size = len(stone)
+        left = stone[0:int(size/2)]
+        right = stone[int(size/2):]
+        right = str(int(right))  # to trim leading zeroes
+        memo[stone] = count(left, blink-1) + count(right, blink-1)
+    else:
+        new = str(int(stone) * 2024)
+        memo[stone] = count(new, blink-1)
+
+    return memo[stone]
+
+
 start_time = time.time()
-for _ in range(25):
-    i = 0
-    while i < len(stones):
-        if stones[i] == '0':
-            stones[i] = '1'
-            i += 1
-        elif len(stones[i]) % 2 == 0:
-            size = len(stones[i])
-            left = stones[i][0:int(size/2)]
-            right = stones[i][int(size/2):]
-            right = str(int(right))  # to trim leading zeroes
-            stones[i] = left
-            stones.insert(i+1, right)
-            i += 2
-        else:
-            stones[i] = str(int(stones[i]) * 2024)
-            i += 1
+total25 = 0
+total75 = 0
+for stone in stones:
+    total25 += count(stone, 25)
+    total75 += count(stone, 75)
+
 
 print(time.time() - start_time, "seconds")
-print(len(stones))
+print("part 1", total25)
+print("part 2", total75)
