356512686b11093b91cc0e2c8df294927f99afef
Violeta Malinova <violeta@mlnova.com>
2024-12-12 10:57:13 +0200

    12 December Part 2

--- 12dec/solution.py
+++ 12dec/solution.py
@@ -7,9 +7,16 @@ n = len(lines)
 m = len(lines[0])
 
 visited = [[False for _ in range(m)] for _ in range(n)]
-areas: dict[int, tuple[int, int]] = {}
+areas: dict[int, tuple[int, int, int]] = {}
 cropId = 0
-idx = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+IDX = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+EDGE_CAPS = [
+    [(-1, 0), (-1, -1), (0, -1)],  # Top Left
+    [(0, -1), (1, -1), (1, 0)],  # Top Right
+    [(1, 0), (1, 1), (0, 1)],  # Bottom Right
+    [(0, 1), (-1, 1), (-1, 0)],  # Bottom Left
+]
 
 
 def getEl(i, j, n, m):
@@ -20,7 +27,23 @@ def getEl(i, j, n, m):
 
 
 def countFences(i, j, n, m):
-    return sum([1 if lines[i][j] != getEl(i+x[0], j+x[1], n, m) else 0 for x in idx])
+    return sum([1 if lines[i][j] != getEl(i+x[0], j+x[1], n, m) else 0 for x in IDX])
+
+
+def countCorners(i, j, n, m):
+    corners = 0
+    for cap in EDGE_CAPS:
+        inside = sum([1 if lines[i][j] == getEl(
+            i+x[0], j+x[1], n, m) else 0 for x in cap])
+        corner_inside = lines[i][j] == getEl(i+cap[1][0], j+cap[1][1], n, m)
+
+        # 1. external corner
+        # 2. external corner pointing to external corner of the same area diagonally
+        # 3. internal corner
+        if inside == 0 or (inside == 1 and corner_inside) or (inside == 2 and not corner_inside):
+            corners += 1
+
+    return corners
 
 
 def walk(i, j, n, m, cropId):
@@ -31,13 +54,14 @@ def walk(i, j, n, m, cropId):
 
     if cropId in areas:
         data = areas[cropId]
-        data = (data[0] + 1, data[1] + countFences(i, j, n, m))
+        data = (data[0] + 1, data[1] + countFences(i, j, n, m),
+                data[2] + countCorners(i, j, n, m))
     else:
-        data = (1, countFences(i, j, n, m))
+        data = (1, countFences(i, j, n, m), countCorners(i, j, n, m))
 
     areas[cropId] = data
 
-    for x in idx:
+    for x in IDX:
         if lines[i][j] == getEl(i+x[0], j+x[1], n, m):
             walk(i+x[0], j+x[1], n, m, cropId)
 
@@ -49,9 +73,12 @@ for i in range(n):
             walk(i, j, n, m, cropId)
             cropId += 1
 
-total = 0
+total1 = 0
+total2 = 0
 for i in range(cropId):
-    total += areas[i][0] * areas[i][1]
+    total1 += areas[i][0] * areas[i][1]
+    total2 += areas[i][0] * areas[i][2]
 
 print(time.time() - start_time, "seconds")
-print(total)
+print("Part 1", total1)
+print("Part 2", total2)
