826b42a0b59c18487dc102de7f756f75e2b1f705
Violeta Malinova <violeta@mlnova.com>
2024-12-13 15:10:03 +0200

    13 Dec Part 2 fast

--- 13dec/solution.py
+++ 13dec/solution.py
@@ -17,7 +17,11 @@ for line in lines:
             xy = line[7:]
 
         x, y = [x[2:] for x in xy.split(", ")]
-        games[gameId].append((int(x), int(y)))
+        if c != 2:
+            games[gameId].append((int(x), int(y)))
+        else:
+            games[gameId].append(
+                (int(x) + 10000000000000, int(y) + 10000000000000))
     else:
         gameId += 1
         c = -1
@@ -25,19 +29,26 @@ for line in lines:
     c += 1
 
 gameId += 1
-mins = [None for _ in range(gameId)]
+total = 0
 for g in range(gameId):
     a = games[g][0]
     b = games[g][1]
     p = games[g][2]
-    for i in range(101):
-        for j in range(101):
-            if a[0]*i + b[0]*j == p[0] and a[1]*i + b[1]*j == p[1]:
-                cost = i*3 + j
-                if mins[g] == None:
-                    mins[g] = cost
-                else:
-                    mins[g] = min(mins[g], cost)
 
-total = sum([m if m != None else 0 for m in mins])
+    p1 = a[1] * p[0]
+    m1 = a[1] * b[0]
+
+    p2 = a[0] * p[1]
+    m2 = a[0] * b[1]
+
+    pt = abs(p1-p2)
+    mt = abs(m1-m2)
+
+    if pt % mt == 0:
+        m = int(pt/mt)
+        diff = p[0] - m*b[0]
+        if diff % a[0] == 0:
+            n = int(diff / a[0])
+            total += (3*n + m)
+
 print(total)
