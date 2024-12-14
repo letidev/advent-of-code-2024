f0a4c322eabca4958a4b2f70e680d97c631c7c7a
Violeta Malinova <violeta@mlnova.com>
2024-12-14 09:54:07 +0200

    14 December Part 2

--- 14dec/solution.py
+++ 14dec/solution.py
@@ -1,4 +1,5 @@
 import math
+import time
 
 with open("test.txt") as f:
     lines = f.read().splitlines()
@@ -26,7 +27,27 @@ for line in lines:
         "v": v
     })
 
+
+def move(robot):
+    p = robot["p"]
+    v = robot["v"]
+
+    move_x = (p[0] + v[0]) % cols
+    move_y = (p[1] + v[1]) % rows
+
+    if move_x < 0:
+        move_x = cols + move_x
+
+    if move_y < 0:
+        move_y = rows + move_y
+
+    p[0] = move_x
+    p[1] = move_y
+
+
+# part 1
 q1, q2, q3, q4 = 0, 0, 0, 0
+start_time = time.time()
 for robot in robots:
     p = robot["p"]
     v = robot["v"]
@@ -52,4 +73,37 @@ for robot in robots:
     if move_x > mid_x and move_y > mid_y:  # bottom right
         q4 += 1
 
+print(time.time() - start_time, "seconds")
 print(q1 * q2 * q3 * q4)
+
+# part 2
+start_time = time.time()
+all_unique = False
+seconds = 0
+
+stars = [[False for _ in range(cols)] for _ in range(rows)]
+
+while not all_unique:
+    arr = []
+    for robot in robots:
+        move(robot)
+        arr.append((robot["p"][0], robot["p"][1]))
+
+    seconds += 1
+    if len(arr) == len(set(arr)):
+        all_unique = True
+
+        for robot in robots:
+            stars[robot["p"][1]][robot["p"][0]] = True
+
+print(time.time() - start_time, "seconds")
+print(seconds)
+
+# print the christmas tree
+for i in range(rows):
+    for j in range(cols):
+        if stars[i][j]:
+            print("*", end="")
+        else:
+            print(".", end="")
+    print()
