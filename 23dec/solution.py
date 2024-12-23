843447d035472f76877d8755372d65d8027dfaae
Violeta Malinova <violeta@mlnova.com>
2024-12-23 16:55:10 +0200

    23 Dec Part 2

--- 23dec/solution.py
+++ 23dec/solution.py
@@ -1,3 +1,5 @@
+import time
+
 with open("test.txt") as f:
     lines = f.read().splitlines()
 
@@ -14,7 +16,8 @@ for line in lines:
     nodes[n1].append(n2)
     nodes[n2].append(n1)
 
-
+# part 1
+start_time = time.time()
 threes = 0
 for node in nodes:
     for i in range(len(nodes[node]) - 1):
@@ -27,3 +30,33 @@ for node in nodes:
 
 threes = int(threes / 3)
 print(threes)
+print(time.time() - start_time, "seconds")
+print()
+
+# part 2
+start_time = time.time()
+C = []
+
+
+def walk(R:  set[str], X:  set[str], P: set[str]):
+    if len(P) == 0 and len(X) == 0 and len(R) > 2:
+        C.append(sorted(R))
+        return
+
+    for v in P.union(set()):
+        walk(R.union([v]), X.intersection(
+            set(nodes[v])), P.intersection(nodes[v]))
+        P.remove(v)
+        X.add(v)
+
+
+walk(set(), set(), set(nodes.keys()))
+
+party = []
+
+for c in C:
+    if len(c) > len(party):
+        party = c
+
+print(",".join(party))
+print(time.time() - start_time, "seconds")
